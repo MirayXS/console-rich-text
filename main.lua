21486c592670cb6880f1b8c6cdc597563c0f51d2
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-09-08 10:47:57 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,23 +3,27 @@
 spawn(function() -- console rich text enabler
     if shared.did then return end
     shared.did = true
+    
     local function f(i,v)
         v = v or i
         if not v:FindFirstChild('msg') then return end
-        v.msg.RichText = true
-        v.msg:GetPropertyChangedSignal('RichText'):Connect(function()
+        local function set(v)
             if v.msg.RichText then return end
             v.msg.RichText = true
-        end)
+        end
+        
+        v.msg.RichText = true
+        v.msg:GetPropertyChangedSignal('RichText'):Connect(set)
+        v.msg:GetPropertyChangedSignal('Visible'):Connect(set)
+        v.msg:GetPropertyChangedSignal('Text'):Connect(set)
     end
-    local a = game:WaitForChild('CoreGui')
     while true do
+        local a = game:WaitForChild('CoreGui')
         a:WaitForChild('DevConsoleMaster'):WaitForChild('DevConsoleWindow')
         local a = a.DevConsoleMaster.DevConsoleWindow:WaitForChild('DevConsoleUI')
-        
         a:WaitForChild('MainView')
         a.MainView:WaitForChild('ClientLog').ChildAdded:Connect(f)
         table.foreach(a.MainView.ClientLog:GetChildren(),f)
-        repeat wait() until not a
+        repeat wait() until not (a and a:FindFirstChild('MainView'))
     end
 end)
